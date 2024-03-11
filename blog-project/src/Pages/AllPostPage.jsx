6e49743ff245f8c7db814692e320c498f968c62e
Tanmay11 <tanmaybhansali07@gmail.com>
2024-03-11 12:49:46 +0530

    Overall the add post feature and the all post view feature is done in… … this project and also the authentication functionality in which if the user is not logged-in then it can access some features only and cannot access the rest of the feature, if user tries to access the feature then it will re-directed to the login page and after doing the login or signup process the user will be redirected to the page from which it was directed to the login page

--- blog-project/src/Pages/AllPostPage.jsx
+++ blog-project/src/Pages/AllPostPage.jsx
@@ -20,7 +20,7 @@ function AllPostPage(){
     }
 
     return(
-        <div className="w-full max-w-full grid gap-y-8 grid-cols-3 pt-2 justify-items-center">
+        <div className="w-full max-w-full grid gap-y-8 grid-cols-3 pt-3 justify-items-center">
             {postSliceData.allPostData.map(individualPost=><Link key={individualPost.$id} to={`/post/${individualPost.$id}`} state={individualPost}><div className="hover:cursor-pointer">
                 <div className="w-[400px] h-[300px] border-4 border-black"><img className="w-full h-full object-cover"  src={storageService.getFilePreview(individualPost.featuredImage)} alt={individualPost.title} /></div>
                 <p className="pt-2 text-center">{individualPost.title}</p>
